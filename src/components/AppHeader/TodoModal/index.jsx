6a900d7f0267db28f20aa0a52188ffb03d341208
Хикмет Садигзаде <sadigzade.hi@mail.ru>
2022-11-12 14:13:33 +0300

    Completely implemented the functionality of the model onk by adding interactive to it, that is, the state of the component

--- src/components/AppHeader/TodoModal/index.jsx
+++ src/components/AppHeader/TodoModal/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineClose } from 'react-icons/md';
 
 import Button from '../../Button';
@@ -6,6 +6,14 @@ import Button from '../../Button';
 import classes from './TodoModal.module.scss';
 
 const TodoModal = ({ modalOpen, setModalOpen }) => {
+  const [title, setTitle] = useState('');
+  const [status, setStatus] = useState('incomplete');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ title, status });
+  };
+
   return (
     modalOpen && (
       <div className={classes.wrapper}>
@@ -13,15 +21,24 @@ const TodoModal = ({ modalOpen, setModalOpen }) => {
           <div className={classes.closeButton} onClick={() => setModalOpen(false)}>
             <MdOutlineClose />
           </div>
-          <form className={classes.form}>
+          <form className={classes.form} onSubmit={(e) => handleSubmit(e)}>
             <h1 className={classes.formTitle}>Add Task</h1>
             <label htmlFor="title">
               Title
-              <input type="text" id="title" />
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </label>
             <label htmlFor="status">
               Status
-              <select name="status" id="status">
+              <select
+                name="status"
+                id="status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}>
                 <option value="incomplete">Incomplete</option>
                 <option value="complete">Complete</option>
               </select>
